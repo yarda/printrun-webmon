8165d744ff9889f8c21264240434637917d02471
Jaroslav Škarvada <jskarvad@redhat.com>
2016-03-11 18:53:47 +0100

    Pronterface status is now also cached
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ $host = "localhost";
 $path = "/";
 $def_port = 7978;
 
-// how long to cache images from webcams in seconds
-$img_cache_delay = 1;
+// how long to cache content in seconds before refresh is needed
+$cache_delay = 1;
 $video_dev_pref = "/dev/video";
 // resolution of images
 $img_res = "320x240";
@@ -22,6 +22,9 @@ $images_in_row = 3;
 // do not probe for more than NUM pronterfaces, under normal conditions
 // it stops probing earlier
 $printers_max = 100;
+$status_cache = "data/status-cache";
+$v4l_lock = "/tmp/printrun-webmon-v4l.lock";
+$status_cache_lock = "/tmp/printrun-webmon-status-cache.lock";
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");   // Date in the past
@@ -36,7 +39,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");   // Date in the past
 <h1 style='text-align:center'>3D printer status</h1>
 <?php
 
-function print_array($arr)
+function print_array($f, $arr)
 {
   $s = "";
   if (!$arr)
@@ -44,18 +47,20 @@ function print_array($arr)
   foreach ($arr as $i)
     $s .= gmdate("H:i:s", round(intval($i))) . "; ";
   if (strlen($s) > 0)
-    echo(substr($s, 0, -2));
+    fprintf($f, "%s", substr($s, 0, -2));
 }
 
 function insert_img($id, $img_res, $delay, $html_prefix, $html_suffix)
 {
+  global $v4l_lock;
+
   $id = strval($id);
   if (!strlen($id))
     return;
   $img = "img/img" . $id . ".jpeg";
   if (!file_exists($img) || (time() - filemtime($img) > $delay))
   {
-    $fp = fopen("/tmp/v4llock", "w");
+    $fp = fopen($v4l_lock, "w");
 
     if (flock($fp, LOCK_EX))
     {
@@ -64,7 +69,7 @@ function insert_img($id, $img_res, $delay, $html_prefix, $html_suffix)
         shell_exec("streamer -c /dev/video" . $id . " -b 16 -s $img_res -o /tmp/img.jpeg");
         rename("/tmp/img.jpeg", $img);
       }
-     flock($fp, LOCK_UN);
+      flock($fp, LOCK_UN);
     }
     fclose($fp);
   }
@@ -76,7 +81,7 @@ function insert_img($id, $img_res, $delay, $html_prefix, $html_suffix)
   }
 }
 
-function insert_status($host, $port, $header)
+function query_pronterface($f, $host, $port, $header)
 {
   $socket = @fsockopen($host, $port, $errno, $errstr);
 
@@ -85,7 +90,7 @@ function insert_status($host, $port, $header)
     if ($errno == 111)
       return 0;
     else
-      echo("<p>Socket error: $errno - $errstr</p>\n");
+      fprintf($f, "$s\n", "<p>Socket error: $errno - $errstr</p>");
     return -1;
   }
 
@@ -100,44 +105,73 @@ function insert_status($host, $port, $header)
   $response = xmlrpc_decode($xml);
 
   if ($response && xmlrpc_is_fault($response))
-    echo("<p>xmlrpc: $response[faultString] ($response[faultCode])</p>\n");
+    fprintf($f, "%s\n", "<p>xmlrpc: $response[faultString] ($response[faultCode])</p>");
   else
   {
-    echo("<table border='border' style='margin-left:auto; margin-right:auto; margin-bottom:1em; min-width:60em; border:1px; text-align:center'>\n");
-    echo("<tr><th colspan='2'>Extruder temp</th><th colspan='2'>Bed temp</th><th rowspan='2'>Progress</th><th rowspan='2'>ETA</th>");
-    echo("<th rowspan='2'>Z</th><th rowspan='2'>Filename</th></tr>\n");
-    echo("<tr><th>Current</th><th>Preset</th><th>Current</th><th>Preset</th></tr>");
-    echo("<tr><td>" . $response["temps"]["T0"][0] . "</td><td>" . $response["temps"]["T0"][1] . "</td>");
-    echo("<td>" . $response["temps"]["B"][0] . "</td><td>" . $response["temps"]["B"][1] . "</td>");
-    echo("<td>");
+    fprintf($f, "%s\n", "<table border='border' style='margin-left:auto; margin-right:auto; margin-bottom:1em; min-width:60em; border:1px; text-align:center'>");
+    fprintf($f, "%s", "<tr><th colspan='2'>Extruder temp</th><th colspan='2'>Bed temp</th><th rowspan='2'>Progress</th><th rowspan='2'>ETA</th>");
+    fprintf($f, "%s\n", "<th rowspan='2'>Z</th><th rowspan='2'>Filename</th></tr>");
+    fprintf($f, "%s", "<tr><th>Current</th><th>Preset</th><th>Current</th><th>Preset</th></tr>");
+    fprintf($f, "%s", "<tr><td>" . $response["temps"]["T0"][0] . "</td><td>" . $response["temps"]["T0"][1] . "</td>");
+    fprintf($f, "%s", "<td>" . $response["temps"]["B"][0] . "</td><td>" . $response["temps"]["B"][1] . "</td>");
+    fprintf($f, "%s", "<td>");
     $progress_f = $response["progress"];
-    printf("%6.2f%%", $progress_f);
-    echo("</td><td>");
-    print_array($response["eta"]);
-    echo("</td><td>");
-    print_r($response["z"]);
-    echo("</td><td>");
-    print_r($response["filename"]);
-    echo("</td></tr></table>\n");
+    fprintf($f, "%6.2f%%", $progress_f);
+    fprintf($f, "%s", "</td><td>");
+    print_array($f, $response["eta"]);
+    fprintf($f, "%s", "</td><td>");
+    fprintf($f, "%s", print_r($response["z"], true));
+    fprintf($f, "%s", "</td><td>");
+    fprintf($f, "%s", print_r($response["filename"], true));
+    fprintf($f, "%s\n", "</td></tr></table>");
   }
   return 1;
 }
 
-$request = xmlrpc_encode_request("status", array());
-$contentlength = strlen($request);
-$reqheader = "POST $path HTTP/1.1\r\n" .
-  "Host: $host\n" . "User-Agent: PHP query\r\n" .
-  "Content-Type: text/xml\r\n".
-  "Content-Length: $contentlength\r\n\r\n".
-  "$request\r\n";
+function insert_status($host, $start_port, $delay, $status_cache)
+{
+  global $status_cache_lock, $printers_max, $path;
 
-$port = $def_port;
-$stat = -1;
-while ($port - $def_port < $printers_max && ($stat = insert_status($host, $port, $reqheader)) != 0)
-  $port++;
-if ($port == $def_port)
-  echo("<p>Unable to connect to Pronterface, maybe it is not running.</p>\n");
+  $fp = fopen($status_cache_lock, "w");
+  if (!file_exists($status_cache) || (time() - filemtime($status_cache) > $delay))
+  {
+    if (flock($fp, LOCK_EX))
+    {
+      if (!file_exists($status_cache) || (time() - filemtime($status_cache) > $delay))
+      {
+        $request = xmlrpc_encode_request("status", array());
+        $contentlength = strlen($request);
+        $reqheader = "POST $path HTTP/1.1\r\n" .
+          "Host: $host\n" . "User-Agent: PHP query\r\n" .
+          "Content-Type: text/xml\r\n".
+          "Content-Length: $contentlength\r\n\r\n".
+          "$request\r\n";
+        $status = array();
+        $port = $start_port;
+        $stat = -1;
+        $f = fopen($status_cache . ".tmp", "w");
+        while ($port - $start_port < $printers_max && ($stat = query_pronterface($f, $host, $port, $reqheader)) != 0)
+          $port++;
+        if ($port == $start_port)
+          fprintf($f, "%s\n", "<p>Unable to connect to Pronterface, maybe it is not running.</p>");
+        fclose($f);
+        rename($status_cache . ".tmp", $status_cache);
+      }
+      flock($fp, LOCK_UN);
+    }
+  }
+  if (flock($fp, LOCK_SH))
+  {
+    $f = fopen($status_cache, "r");
+    while (($buf = fgets($f, 4096)) !== false)
+      echo($buf);
+    fclose($f);
+    flock($fp, LOCK_UN);
+  }
+  fclose($fp);
+}
 
+insert_status($host, $def_port, $cache_delay, $status_cache);
 $vdp_len = strlen($video_dev_pref);
 $cnt = 0;
 echo("<table style='margin-left:auto; margin-right:auto; text-align:center; border-spacing:1em'>\n");
@@ -145,7 +179,7 @@ foreach (glob($video_dev_pref . "[0-9]*") as $f)
 {
   if ($cnt % $images_in_row == 0)
     echo("<tr>");
-  insert_img(substr($f, $vdp_len), $img_res, $img_cache_delay, "<td>", "</td>\n");
+  insert_img(substr($f, $vdp_len), $img_res, $cache_delay, "<td>", "</td>\n");
   $cnt++;
   if ($cnt % $images_in_row == 0)
     echo("</tr>\n");
