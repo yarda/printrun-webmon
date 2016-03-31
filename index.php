ce937b27b5f67d7b55ba2b69f394dd071e710742
Jaroslav Škarvada <jskarvad@redhat.com>
2016-03-31 17:25:39 +0200

    Default cache delay decreased to 2 seconds
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $path = "/";
 $def_port = 7978;
 
 // how long to cache content in seconds before refresh is needed
-$cache_delay = 5;
+$cache_delay = 2;
 $video_dev_pref = "/dev/video";
 $page_title = "3D printer status";
 // resolution of images
