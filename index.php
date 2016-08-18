6bb5cc21a5afb54f087363726dfe86191850dcef
Jaroslav Škarvada <jskarvad@redhat.com>
2016-08-18 14:26:19 +0200

    Added comment to 'pragma: no-cache'
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $status_cache = "data/status-cache";
 $v4l_lock = "/tmp/printrun-webmon-v4l.lock";
 $status_cache_lock = "/tmp/printrun-webmon-status-cache.lock";
 
-header("Pragma: no-cache");
+header("Pragma: no-cache"); // HTTP/1.0
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");   // Date in the past
 
