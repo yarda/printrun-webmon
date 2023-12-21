4599904ecc5b661c96e5bbf2f672eb696bdbc91e
Harvey Lynden <harveylynden@gmail.com>
2023-12-21 12:44:40 +0100

    Timeout in case process hangs 30 seconds kill

--- camera.php
+++ camera.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * This is an KISS (Keep It Simple and Stupid) web monitor for
+ * This is a KISS (Keep It Simple and Stupid) web monitor for
  * printrun / pronterface written in PHP.
  * Copyright (C) 2016  Jaroslav Škarvada <jskarvad@redhat.com>
- * The code is copied under GPLv3+. Use it on your own risk.
+ * The code is copied under GPLv3+. Use it at your own risk.
  */
 
 require("config.php");
@@ -18,29 +18,32 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header("Location: {$img}");
-ob_flush(); flush();
+ob_flush();
+flush();
 
 $cameras = getCameras($video_dev_glob, $rulesets);
 if (array_key_exists(strval($id), $cameras)) {
   $camera = $cameras[$id];
 
-  // generate new shot after redirect
-  if (!file_exists($img) || (time() - filemtime($img) > $delay))
-  {
+  // Set a timeout value in seconds
+  $timeout = 30;
+
+  // Generate a new shot after the redirect
+  if (!file_exists($img) || (time() - filemtime($img) > $delay)) {
     $fp = fopen($v4l_lock, "w");
-    if (flock($fp, LOCK_EX))
-    {
-      if (!file_exists($img) || (time() - filemtime($img) > $delay))
-      {
+    if (flock($fp, LOCK_EX)) {
+      if (!file_exists($img) || (time() - filemtime($img) > $delay)) {
         if (!isset($camera["fswebcam_args"])) {
-          $command = "streamer -q -c {$camera['devicePath']} -b 16 -s {$img_res} -o {$tmp}";
+          $command = "timeout {$timeout}s streamer -q -c {$camera['devicePath']} -b 16 -s {$img_res} -o {$tmp}";
         } else {
           $args = array_map('escapeshellarg', $camera["fswebcam_args"]);
-          $command = "fswebcam -q --no-banner --no-timestamp --device {$camera['devicePath']} --resolution {$img_res} --save {$tmp} ". implode(' ', $args);
+          $command = "timeout {$timeout}s fswebcam -q --no-banner --no-timestamp --device {$camera['devicePath']} --resolution {$img_res} --save {$tmp} " . implode(' ', $args);
         }
-	exec($command, $output, $retval);
-        if ($retval == 0)
-        {
+
+        exec($command, $output, $retval);
+
+        // Check if the image file exists and rename it
+        if (file_exists($tmp)) {
           rename($tmp, $img);
         }
       }
